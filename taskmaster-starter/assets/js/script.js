2ae0eb03efc5dd27a5c8f3a11934b5cb27287373
Mai Yia Xiong <maiyiax@gmail.com>
2020-10-04 05:57:34 -0500

    added moment.js elements

--- taskmaster-starter/assets/js/script.js
+++ taskmaster-starter/assets/js/script.js
@@ -13,6 +13,9 @@ var createTask = function (taskText, taskDate, taskList) {
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
 
+  // check due date
+  auditTask(taskLi);
+
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -103,12 +106,21 @@ $(".list-group").on("click", "span", function () {
   // swap out elements
   $(this).replaceWith(dateInput);
 
+  // enable jquery ui datepicker
+  dateInput.datepicker({
+    minDate: 1,
+    onClose: function () {
+      // when calendar is closed, force a "change" event on the 'dateInput'
+      $(this).trigger("change");
+    }
+  });
+
   // automatically focus on new element
   dateInput.trigger("focus");
 });
 
 // blur event after value of date was changed
-$(".list-group").on("blur", "input[type='text']", function () {
+$(".list-group").on("change", "input[type='text']", function () {
   // get current text
   var date = $(this)
     .val()
@@ -136,6 +148,9 @@ $(".list-group").on("blur", "input[type='text']", function () {
 
   // replace input with span element
   $(this).replaceWith(taskSpan);
+
+  // Pass task's <li> element into auditTask() to check new due date
+  auditTask($(taskSpan).closest(".list-group-item"));
 });
 
 // make <ul> element list-group sortable with jQuery UI
@@ -189,6 +204,7 @@ $(".card .list-group")
     }
   });
 
+// add trash field
 $("#trash")
   .droppable({
     accept: ".card .list-group-item",
@@ -204,6 +220,40 @@ $("#trash")
       console.log("out");
     }
   });
+
+// add datepicker to modal
+$("#modalDueDate").datepicker({
+  minDate: 1
+});
+
+
+var auditTask = function (taskEl) {
+  // get date from task element
+  var date = $(taskEl)
+    .find("span")
+    .text()
+    .trim();
+  console.log(date);
+
+  // convert to moment object at 5:00pm
+  var time = moment(date, "L")
+    .set("hour", 17);
+  
+  // remove any old classes from element
+  $(taskEl)
+    .removeClass("list-group-item-warning list-group-item-danger");
+
+  // apply new class if task is near/over due date
+  if (moment()
+    .isAfter(time)) {
+      $(taskEl)
+        .addClass("list-group-item-danger");
+    }
+    else if (Math.abs(moment().diff(time, "days")) <= 2) {
+      $(taskEl)
+        .addClass("list-group-item-warning");
+    }
+};
 
 // modal was triggered
 $("#task-form-modal").on("show.bs.modal", function () {
